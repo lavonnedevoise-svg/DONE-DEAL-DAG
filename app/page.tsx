f148b211ad9b70fd965448a29f6dd6c410f49076
lavonnedevoise-svg <lavonnedevoise@gmail.com>
2025-12-22 22:04:31 -0500

    Create Home component with introductory content

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+export default function Home() {
+return (
+<main style={{ padding: "48px", fontFamily: "system-ui" }}>
+<h1 style={{ fontSize: "42px", marginBottom: "12px" }}>
+DeVoise Ascension Group
+</h1>
+<p style={{ fontSize: "18px", maxWidth: "720px", lineHeight: 1.6 }}>
+We take you higher.
+</p>
+
+<div style={{ marginTop: "24px" }}>
+<a
+href="https://calendly.com/lavonnedevoise/30min"
+style={{
+display: "inline-block",
+padding: "12px 18px",
+borderRadius: "10px",
+background: "#111",
+color: "#fff",
+textDecoration: "none",
+fontWeight: 600,
+}}
+>
+Book a Call
+</a>
+</div>
+</main>
+);
+}
